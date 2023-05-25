a806d4fcd8ddd5907537d01bf40854641ef2d631
asadbek-madaminov <dasturchi.madaminov@gmail.com>
2023-05-25 22:59:15 +0500

    qabul rad etish

--- app/src/main/java/uz/ibrohim/sovchiuz/more_page/offer/CameActivity.kt
+++ app/src/main/java/uz/ibrohim/sovchiuz/more_page/offer/CameActivity.kt
@@ -3,8 +3,9 @@ package uz.ibrohim.sovchiuz.more_page.offer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import uz.ibrohim.sovchiuz.databinding.ActivityCameBinding
+import uz.ibrohim.sovchiuz.ui.modul.TaklifData
 
-class CameActivity : AppCompatActivity() {
+class CameActivity : AppCompatActivity(),CameAdapter.click {
 
     private lateinit var binding: ActivityCameBinding
 
@@ -12,5 +13,13 @@ class CameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onlistern(data: TaklifData, listern: Int) {
+        if (listern == 1){
+            //qabul
+        }else{
+            //rad etildi
+        }
     }
 }
